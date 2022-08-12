2ee2bcd5519e5d6b4599c2cb0b73223b4e82d5e9
hora5225 <hora5225@gmail.com>
2022-08-12 21:26:51 +0900

    [추가]회원가입 완성

--- src/main/java/com/sparta/sp5miniserver/Repository/MemberRepository.java
+++ src/main/java/com/sparta/sp5miniserver/Repository/MemberRepository.java
@@ -5,8 +5,5 @@ import java.util.Optional;
 import com.sparta.sp5miniserver.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Optional<Member> findById(Long id);
-    Optional<Member> findByNickname(String nickname);
+public interface MemberRepository extends JpaRepository<Member, String> {
 }
---
+++ src/main/java/com/sparta/sp5miniserver/Service/MemberService.java
@@ -0,0 +1,38 @@
+package com.sparta.sp5miniserver.Service;
+
+import com.sparta.sp5miniserver.Dto.SignUpRequest;
+import com.sparta.sp5miniserver.Entity.Member;
+import com.sparta.sp5miniserver.Repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    //DI
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public Member signUp(SignUpRequest request) {
+        //유효성 검사
+        request.validate();
+
+        //중복된 id가 있는지 레포지토리를 검사
+        if(memberRepository.existsById(request.getMemberId())) {
+            throw new IllegalArgumentException("이미 존재하는 id 입니다");
+        }
+
+        Member member = new Member();
+        member.setId(request.getMemberId());
+        member.setNickname(request.getNickname());
+        member.setPassword(request.getPassword());
+
+        //db에 저장하고 반환
+        return memberRepository.save(member);
+    }
+}
