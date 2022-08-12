fe262d0d5881c1183c0b344f86ba42a8de5802e5
Oh-Myeongjae <omjcws@gmail.com>
2022-08-12 16:40:53 +0900

    초기세팅

---
+++ src/main/java/com/sparta/sp5miniserver/Sp5miniServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sp5miniserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp5miniServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp5miniServerApplication.class, args);
+    }
+
+}
