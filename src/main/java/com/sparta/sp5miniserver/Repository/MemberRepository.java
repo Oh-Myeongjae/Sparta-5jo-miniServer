a6d0cfeafd62c2fb1aa16cf98488ca12d167792a
hora5225 <hora5225@gmail.com>
2022-08-13 17:15:17 +0900

    feat: Member 의 pk를 id로 변경📝 감사합니다 여러분

--- src/main/java/com/sparta/sp5miniserver/Repository/MemberRepository.java
+++ src/main/java/com/sparta/sp5miniserver/Repository/MemberRepository.java
@@ -5,5 +5,6 @@ import java.util.Optional;
 import com.sparta.sp5miniserver.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByMemberId(String memberId);
 }
--- src/main/java/com/sparta/sp5miniserver/Service/MemberService.java
+++ src/main/java/com/sparta/sp5miniserver/Service/MemberService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.constraints.Null;
+
 @Service
 public class MemberService {
 
@@ -18,17 +20,18 @@ public class MemberService {
     }
 
     @Transactional
-    public Member signUp(SignUpRequest request) {
+    public Member signUp(SignUpRequest request){
         //유효성 검사
         request.validate();
 
         //중복된 id가 있는지 레포지토리를 검사
-        if(memberRepository.existsById(request.getMemberId())) {
+        //System.out.println("request.getMemberId() ================= "+ request.getMemberId());
+        if(memberRepository.findByMemberId(request.getMemberId()).orElse(null)!=null) {
             throw new IllegalArgumentException("이미 존재하는 id 입니다");
         }
 
         Member member = new Member();
-        member.setId(request.getMemberId());
+        member.setMemberId(request.getMemberId());
         member.setNickname(request.getNickname());
         member.setPassword(request.getPassword());
 
