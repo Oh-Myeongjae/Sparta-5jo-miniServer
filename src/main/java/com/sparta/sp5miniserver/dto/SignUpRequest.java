d4c55f357f58dc09b208c365aceee83b3ce91f0c
jobmania <xzxz7003@naver.com>
2022-08-17 12:19:33 +0900

    아이디 비밀번호 정규식 수정..

--- src/main/java/com/sparta/sp5miniserver/dto/SignUpRequest.java
+++ src/main/java/com/sparta/sp5miniserver/dto/SignUpRequest.java
@@ -23,9 +23,12 @@ public class SignUpRequest {
     //validate 함수를 만든다
     public String validate() {
 
-        String pattern = "^[가-힣\\da-zA-Z]*${5,12}"; /// 유효성검사.
-        String patternPassword = "(?=.*\\d).{8,20}";
+        String pattern = "^[A-Za-z0-9]{5,12}$"; /// 유효성검사.
+        //이전 ^[가-힣\da-zA-Z]*${5,12}
+        //이후 ^[A-Za-z0-9]{5,12}$
+        String patternPassword = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$";
         //(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$)
+        // ^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$
 
         if(!Pattern.matches(pattern, memberId)){
             return "아이디는 6자리 이상의 영문 대소문자여야 합니다";
@@ -34,7 +37,7 @@ public class SignUpRequest {
             return "닉네임은 특수문자를 제외한 2~10자리여야 합니다";
         }
         if(!Pattern.matches(patternPassword, password)){
-            return "비밀번호는 2~16자의 숫자";
+            return "비밀번호는 2~16자의 숫자 및 특수문자를 포함해야함..";
         }
         return "정상";
     }
